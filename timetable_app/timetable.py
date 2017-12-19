e5f5b590f42772f3c6f72adc83dee6c1ab0b84f7
kmazi <ugochukwu.mazi@andela.com>
2017-12-19 20:53:48 +0100

    chore(timetable): Generate timetable  - generate the timetable for lectures  - show timetable for lecturers

--- timetable_app/timetable.py
+++ timetable_app/timetable.py
@@ -1,4 +1,4 @@
-
+from datetime import timedelta
 from .models import TimeSlot
 
 
@@ -14,6 +14,7 @@ class Timetable(object):
 
     def get_time_slots(self, lecture_times):
         time_slots = dict()
+        new_slots = dict()
         mon_lectures, tue_lectures = list(), list()
         wed_lectures, thu_lectures = list(), list()
         fri_lectures = list()
@@ -33,17 +34,36 @@ class Timetable(object):
             if lecture_time.day.name == "friday":
                 fri_lectures.append(lecture_time)
                 time_slots["friday"] = fri_lectures
-        return time_slots
+        for day, time_slot in time_slots.items():
+            i = 0
+            lectures = list()
+            next_count = 8
+            for count in range(8, 18):
+                length = len(time_slot)
+                if length > i and time_slot[i].start_time.hour == count:
+                    lectures.append(time_slot[i])
+                    if time_slot[i].duration == timedelta(hours=2):
+                        next_count += 2
+                    i+=1
+                else:
+                    if next_count == count:
+                        lectures.append("")
+                        next_count += 1
+            new_slots[day] = lectures
+        return new_slots
 
     def generate_timetable(self):
         timetable = dict()
         if self.type == "lecturer":
-            time_slots = TimeSlot.objects.filter(course__lecturer=self.lecturer)
+            time_slots = TimeSlot.objects.filter(course__lecturer=self.lecturer)\
+                                              .order_by('day', 'start_time')
         if self.type == "set":
             time_slots = TimeSlot.objects.filter(course__level=self.level,
-                                                 course__department__name=self.department)
+                                                 course__department__name=self.department)\
+                                                 .order_by('day', 'start_time')
         if self.type == "department":
-            time_slots = TimeSlot.objects.filter(course__department__name=self.department)
+            time_slots = TimeSlot.objects.filter(course__department__name=self.department)\
+                                                .order_by('day', 'start_time')
         if len(time_slots) > 0:
             timetable = self.get_time_slots(time_slots)
         return timetable
--- timetable_app/views.py
+++ timetable_app/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
+from .models import TimeSlot
+from .timetable import Timetable 
 
 class HomePage(TemplateView):
     template_name = "homepage.html"
+
+class ShowTable(TemplateView):
+    template_name = "timetable.html"
+
+    def get_context_data(self, **kwargs):
+        lecturer = self.request.user
+        timetable = Timetable(timetable_for="lecturer", lecturer=lecturer).generate_timetable()
+        context = super(ShowTable, self).get_context_data(**kwargs)
+        context['timetable'] = timetable
+        return context
+
