d5c9ffecd494486df663f678323795c69c33b524
kmazi <ugochukwu.mazi@andela.com>
2018-01-11 06:31:11 +0100

    chore(authentication) user authentication  - check if user is authenticated to show logout button

--- timetable_app/urls.py
+++ timetable_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.HomePage.as_view()),
     url(r'^timetable/$', views.ShowTable.as_view()),
+    url(r'^timetable/(?P<department_id>\d)/(?P<level>\d)/$', views.LevelTimetable.as_view()),
     url(r'^login/$', auth_views.login),
     url(r'^logout/$', auth_views.logout, {'next_page':'/'}, name='logout'),
 ]
--- timetable_app/views.py
+++ timetable_app/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
-from .models import TimeSlot
-from .timetable import Timetable 
+from .models import TimeSlot, Department
+from .timetable import Timetable
 
 class HomePage(TemplateView):
     template_name = "homepage.html"
@@ -16,3 +16,15 @@ class ShowTable(TemplateView):
         context['timetable'] = timetable
         return context
 
+class LevelTimetable(TemplateView):
+    template_name = "timetable.html"
+
+    def get_context_data(self, **kwargs):
+        lecturer = self.request.user
+        department = Department.objects.filter(id=int(self.kwargs['department_id']))
+        level = int(self.kwargs['level'])
+        timetable = Timetable(timetable_for="set", department=department).generate_timetable()
+        context = super(LevelTimetable, self).get_context_data(**kwargs)
+        context['departments'] = departments
+        context['timetable'] = timetable
+        return context
